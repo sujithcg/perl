70f0e293b2d06db09d0c1b19859c3e86d67bbf72
sujithcg <sujithnair99.it@gmail.com>
2021-05-05 16:20:45 +0530

    add Perlprogram

---
+++ Perl_Program.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+  
+
+@arr1 = (1, 2, 3, 4);
+  
+
+@arr2 = qw /Automation Academy Certification Program/; 
+  
+
+print "Elements of arr1 are:\n"; 
+print "$arr1[0]\n";
+print "$arr1[3]\n";
+  
+
+print "\nElements of arr2 are:\n"; 
+print "$arr2[-4]\n";
+print "$arr2[-3]\n";
+print "$arr2[-2]\n";
+print "$arr2[-1]\n";
